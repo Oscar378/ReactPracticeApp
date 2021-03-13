1f3c58419e216fb8592f6fa0d51ece6a81a496d1
Oscar378 <oscar.villarruel9@gmail.com>
2021-03-13 11:13:08 -0800

    Finished Header for Home page version.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,23 +1,26 @@
 import React from "react";
+import Header from '../sections/header.jsx';
 import '../style.css';
 
 function Home() {
-
   return (
-  <div className="mainContainer">
-    <div className="row" style={{marginTop: 50}}>
-      <div className="col-lg-12 col-md-12 col-sm-12">
-        <div className="buttonLayout center">
-          <div id="pag_home_newTicketButton" className="button normal">
-            New Order
-          </div>
-          <div id="pag_home_existTicketButton" className="button normal">
-            Existing Order
+    <>
+      <Header />
+      <div className="mainContainer">
+        <div className="row" style={{marginTop: 50}}>
+          <div className="col-lg-12 col-md-12 col-sm-12">
+            <div className="buttonLayout center">
+              <div id="pag_home_newTicketButton" className="button normal">
+                New Order
+              </div>
+              <div id="pag_home_existTicketButton" className="button normal">
+                Existing Order
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
+    </>
   );
 }
 
--- src/sections/header.jsx
+++ src/sections/header.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import '../style.css';
 
 function Header() {
-
   return (
-
+    <div className="header homeText">
+        <div className="title homeText">
+          <h1>Home</h1>
+        </div>
+    </div>
   );
 }
 
