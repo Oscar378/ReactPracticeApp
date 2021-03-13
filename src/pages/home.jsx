23ffcb30f39c2d5865f08dd256adf2ab0947d786
Oscar378 <oscar.villarruel9@gmail.com>
2021-03-13 09:42:37 -0800

    Finished Home Page. Started on Header.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import '../style.css';
 
 function Home() {
 
   return (
-    <>
-    Home Page
-    </>
+  <div className="mainContainer">
+    <div className="row" style={{marginTop: 50}}>
+      <div className="col-lg-12 col-md-12 col-sm-12">
+        <div className="buttonLayout center">
+          <div id="pag_home_newTicketButton" className="button normal">
+            New Order
+          </div>
+          <div id="pag_home_existTicketButton" className="button normal">
+            Existing Order
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   );
 }
 
